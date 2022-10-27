e467124cfd2f2a548004237bb9dc8350b597b572
HeiseMo <ttanurhan@DESKTOP-8NVOKRQ.localdomain>
2022-10-27 15:42:46 +0200

    removed env

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-router.get("/", passport.authenticate("discord"));
+//router.get("/", passport.authenticate("discord"));
 router.get(
   "/redirect",
   passport.authenticate("discord", {
